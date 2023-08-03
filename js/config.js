992f12a9288429850399a4a61084695ea1a893ac
jalvarez@dt.gob.cl <jalvarez@dt.gob.cl>
2023-08-03 18:26:46 -0400

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_Ttpy8vOvW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '73eb37jh6drt5ohfmsgrkv1boq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
